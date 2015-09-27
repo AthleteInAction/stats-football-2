e8b521f72b9989b6a70676f4765f4f14372e2bfd
Will Robinson <wrobinson-web-contractor@evernote.com>
2015-09-27 03:54:54 -0700

    INITIAL

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+use_frameworks!
+
+target 'stats-football-2' do
+
+	pod 'Parse'
+	pod 'Alamofire'
+	pod 'SwiftyJSON'
+
+end
+
+target 'stats-football-2Tests' do
+
+end
---
+++ stats-football-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stats-football-2
+//
+//  Created by grobinson on 9/27/15.
+//  Copyright (c) 2015 Wambl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
